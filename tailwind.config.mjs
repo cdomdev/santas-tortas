69a0dfd580b48b7e4ed5e9ef77524ae2c59e1900
cdom <dominguez5493.cd@gmail.com>
2024-12-02 16:13:43 -0500

    refactor component navbar

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,12 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			backgroundImage: {
-				"wave-image": "url('/bg-header.png')",
-			  },
-		},
-	},
-	plugins: [require("flowbite/plugin")],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("flowbite/plugin")],
+};
