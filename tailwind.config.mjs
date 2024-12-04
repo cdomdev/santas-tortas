3c9288267cb0572a3afaa029be9d764c6974730c
dominguez5493.cd <dominguez5493.cd@gmail.com>
2024-12-04 08:18:30 +0000

    faect: add and setting compoenents

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,19 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        "primary-bg": "#f2eae1",
+        "secondary-bg": "#fc9fa2",
+      },
+      backgroundImage: {
+        gradient:
+          "linear-gradient(90deg, rgba(255,136,244,1) 20%, rgba(231,55,211,1) 63%, rgba(228,30,206,1) 88%, rgba(221,43,223,1) 100%);",
+      },
+    },
+    colors: {
+      "primary-txt": "#5f3b3c",
+    },
   },
   plugins: [require("flowbite/plugin")],
 };
