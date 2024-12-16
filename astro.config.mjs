b1d0451933db3ab262963aac64a228b596fb2998
cdom <dominguez5493.cd@gmail.com>
2024-12-16 17:14:37 -0500

    update astro.confg.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
+  output: 'server',
   devToolbar:{
     enabled: false
   }
