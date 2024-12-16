c5f1ceb1f6f85a6bb72582d9102d30a4ed6525bf
cdom <dominguez5493.cd@gmail.com>
2024-12-16 17:23:54 -0500

    update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ import vercel from "@astrojs/vercel/serverless"
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  output: 'server',
+  output: 'static',
   adapter: vercel(),
   devToolbar:{
     enabled: false
