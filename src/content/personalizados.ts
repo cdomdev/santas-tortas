09289a872797e9914bb214750a4f9f52958a1c0f
cdomdev <dominguez5493.cd@gmail.com>
2025-01-14 16:00:07 -0500

    nuevo archivo para el contenido de la pagina de personlizados

---
+++ src/content/personalizados.ts
@@ -0,0 +1,42 @@
+export const items = [
+  {
+    image: "/carrusel/img1.webp",
+    title: "Pasteleria personalizada",
+    text: "Este es el lugar perfecto para hacer realidad tus antojos, aqui encontraras postres y galletas, ademas de la psibilida de hacer realidad ese diseño que tienes en mente, solicita tu pastel personlizado aqui",
+    thumbnail: {
+      image: "",
+    },
+  },
+  {
+    image: "/carrusel/img1.webp",
+    title: "ChocoChip Delicia",
+    text: " Pastel de chocolate intenso y húmedo, salpicado de chispas de chocolate que se derriten en cada bocado. Perfecto para los amantes del chocolate.",
+    thumbnail: {
+      image: "/imgthum4.webp",
+    },
+  },
+  {
+    image: "/carrusel/img2.webp",
+    title: "Tres Leches Frutal",
+    text: "Tradicional pastel de tres leches, bañado en una mezcla dulce y cremosa, coronado con crema batida y fresas frescas. Una experiencia fresca y decadente.",
+    thumbnail: {
+      image: "/imgthum1.webp",
+    },
+  },
+  {
+    image: "/carrusel/img3.webp",
+    title: "Dulce Encanto de Vainilla",
+    text: " Delicioso pastel de vainilla suave y esponjoso, cubierto con crema batida y decorado con frescas. Un clásico irresistible para cualquier ocasión especial.",
+    thumbnail: {
+      image: "/imgthum2.webp",
+    },
+  },
+  {
+    image: "/carrusel/img4.webp",
+    title: "Sueño de Chocolate y Crema",
+    text: "Pastel de chocolate rico y esponjoso, con un suave relleno de crema batida, decorado con una mezcla de cacao en polvo y toques de crema para un acabado elegante y delicioso.",
+    thumbnail: {
+      image: "/imgthum3.webp",
+    },
+  },
+];
