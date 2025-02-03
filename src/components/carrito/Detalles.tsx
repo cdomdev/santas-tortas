c309a86918aa02394a69c817683a4bc05210ad9b
dominguez5493.cd <dominguez5493.cd@gmail.com>
2025-02-03 17:35:06 +0000

    style: ajuste del padding en las celda de datos de usuario

--- src/components/carrito/Detalles.tsx
+++ src/components/carrito/Detalles.tsx
@@ -1,4 +1,4 @@
-import  type{ Producto, Usuario} from "@/types";
+import type { Producto, Usuario } from "@/types";
 import { useState, useEffect } from "react";
 import {
   calcularTotal,
@@ -6,38 +6,43 @@ import {
   calcularSubTotal,
 } from "@/utils/productos";
 
-
 export const Detalles = () => {
   const [productos, setProductos] = useState<Producto[]>([]);
+  const [envio, setEnvio] = useState<string | null>(null);
+  let valorEnvio = 15000;
   useEffect(() => {
     const carrito = JSON.parse(localStorage.getItem("carrito") || "[]");
     setProductos(carrito);
+
+    const datosEnvio = localStorage.getItem("value-car-forsend");
+    if (datosEnvio) {
+      setEnvio(datosEnvio);
+    }
   }, []);
 
-  const envio = localStorage.getItem("send-value-car") || 0;
-  const subTotalText = calcularTotal(productos).toString();
-  const SubtotalInt = formateValue(subTotalText);
+  const totalConEnvio = calcularTotal(productos) + valorEnvio;
+  const SubtotalInt = formateValue(totalConEnvio.toString());
 
   return (
     <>
-      <h3 className="text-center md:text-lg font-semibold text-gray-600 uppercase py-2">
+      <h2 className="text-center md:text-lg font-semibold text-gray-600 uppercase py-2">
         Este es un resumen de tu compra
-      </h3>
+      </h2>
       <div className="border">
         <div className="relative overflow-x-auto">
           <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
             <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
               <tr>
-                <th scope="col" className="px-4 py-3">
+                <th scope="col" className="px-2 md:px-4 py-3">
                   Nombre
                 </th>
-                <th scope="col" className="px-4 py-3">
+                <th scope="col" className="px-2 md:px-4 py-3">
                   Descuento
                 </th>
-                <th scope="col" className="px-4 py-3">
+                <th scope="col" className="px-2 md:px-4 py-3">
                   Cantidad
                 </th>
-                <th scope="col" className="px-4 py-3">
+                <th scope="col" className="px-2 md:px-4 py-3">
                   Precio
                 </th>
               </tr>
@@ -48,21 +53,23 @@ export const Detalles = () => {
                   key={producto.id}
                   className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200"
                 >
-                  <td className="px-6 py-4">{producto.nombre}</td>
-                  <td className="px-6 py-4 text-red-600">
+                  <td className="px-4 md:px-6 py-4">{producto.nombre}</td>
+                  <td className="px-4 md:px-6 py-4 text-red-600">
                     {producto.descuento}%
                   </td>
-                  <td className="px-6 py-4">{producto.quantity}</td>
-                  <td className="px-6 py-4">{calcularSubTotal(producto)}</td>
+                  <td className="px-4 md:px-6 py-4">{producto.quantity}</td>
+                  <td className="px-4 md:px-6 py-4">{calcularSubTotal(producto)}</td>
                 </tr>
               ))}
 
               {envio ? (
                 <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
                   <td className="px-6 py-4 text-black font-semibold">Envio</td>
-                  <td className="px-6 py-4"></td>
-                  <td className="px-6 py-4"></td>
-                  <td className="px-6 py-4">{formateValue(envio)}</td>
+                  <td className="px-4 md:px-6 py-4"></td>
+                  <td className="px-4 md:px-6 py-4"></td>
+                  <td className="px-4 md:px-6 py-4">
+                    {formateValue(valorEnvio.toString())}
+                  </td>
                 </tr>
               ) : null}
             </tbody>
@@ -89,11 +96,16 @@ export const Detalles = () => {
   );
 };
 
+
+
+
 export const DetallesUsuario = () => {
   const [datos, setDatos] = useState<Usuario>();
+
+  console.log(datos);
   useEffect(() => {
-    const carrito = JSON.parse(localStorage.getItem("data-send-user") || "[]");
-    setDatos(carrito);
+    const datos = JSON.parse(localStorage.getItem("data") || "[]");
+    setDatos(datos);
   }, []);
 
   return (
@@ -102,49 +114,36 @@ export const DetallesUsuario = () => {
         <div className="relative overflow-x-auto">
           <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
             <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-              <tr>
+              <tr className="border-b">
                 <th scope="col" className="px-4 py-3">
-                  Nombre
+                  Nombres
+                </th>
+                <th scope="col" className="px-4 py-3 border-r border-l ">
+                  Apellidos
+                </th>
+                <th scope="col" className="px-4 py-3 border-r border-l ">
+                  Telefono
                 </th>
                 <th scope="col" className="px-4 py-3">
-                  Descuento
-                </th>
-                <th scope="col" className="px-4 py-3">
-                  Cantidad
-                </th>
-                <th scope="col" className="px-4 py-3">
-                  Precio
+                  Direccion
                 </th>
               </tr>
             </thead>
             <tbody>
-              {datos
-                ? datos.map((dato) => (
-                    <tr
-                      key={dato.id}
-                      className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200"
-                    >
-                      <td className="px-6 py-4">{dato.nombre}</td>
-                      <td className="px-6 py-4 text-red-600">{dato}%</td>
-                      <td className="px-6 py-4">{}</td>
-                    </tr>
-                  ))
-                : null}
-            </tbody>
-
-            <tfoot>
-              <tr className="bg-white dark:bg-gray-800">
-                <th
-                  scope="row"
-                  className="px-6 py-4 font-bold text-gray-900 whitespace-nowrap dark:text-white"
-                >
-                  Total
-                </th>
-                <td className="px-6 py-4"></td>
-                <td className="px-6 py-4"></td>
+              <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
+                <td className="px-2 py-4">{datos?.nombre}</td>
+                <td className="px-2 py-4">{datos?.apellidos}</td>
+                <td className="px-2 py-4">{datos?.telefono}</td>
+                <td className="px-2 py-4">{datos?.direccion}</td>
               </tr>
-            </tfoot>
+            </tbody>
           </table>
+          <div className="flex w-full flex-col">
+            <p className="text-center font-semibold text-gray-600 pt-1 border-b">
+              Datos adicionales
+            </p>
+            <p className=" text-gray-600 text-sm py-3 text-balance px-6">{datos?.message}</p>
+          </div>
         </div>
       </div>
     </>
