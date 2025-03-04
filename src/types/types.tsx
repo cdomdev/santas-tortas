4657fb8d670415b7ecf7b884038d399d11694442
Carlos <dominguez5493.cd@gmail.com>
2025-03-04 18:09:26 -0500

    refactor: ajuste de tipo de datos

--- src/types/types.tsx
+++ src/types/types.tsx
@@ -1,37 +1,49 @@
-export type UUID = `${string}-${string}-${string}-${string}-${string}`
+export type UUID = `${string}-${string}-${string}-${string}-${string}`;
 
 export type Producto = {
-    id: UUID;
-    nombre: string;
-    imagen: string;
-    precio: string;
-    descuento: number | null;
-    descripcion: string;
-    referencia: string | number;
-    categoria: any | null;
-    quantity?: number
-  }
-  
-  export type Usuario = {
-    id?: UUID;
-    nombre: string;
-    apellidos?: string;
-    email: string;
-    telefono?: string;
-    direccion?: string;
-    ciudad?: string;
-    estado?: string;
-    message?: string;
-    codigoPostal?: string;
-  }
+  id: number | string;
+  title: string;
+  price: string;
+  discount: number | null;
+  description: string;
+  quantity?: number;
+  slug: string
+  images: Images[];
+  category: {
+    id: number;
+    documentId: string;
+    name: string;
+  } 
+};
+
+export type Images = {
+  id: number;
+  documentId: string;
+  url: string;
+};
 
 
- export  type Personalizado = {
-    tematica: string;
-    relleno: string;
-    sabor: string;
-    fecha: string;
-    porciones: string;
-    foto?: File | null;
-    mensaje: string;
-  };
+
+export type Usuario = {
+  id?: UUID;
+  nombre: string;
+  apellidos?: string;
+  email: string;
+  telefono?: string;
+  direccion?: string;
+  ciudad?: string;
+  estado?: string;
+  message?: string;
+  codigoPostal?: string;
+};
+
+export type Personalizado = {
+  tematica: string;
+  relleno: string;
+  sabor: string;
+  fecha: string;
+  porciones: string;
+  foto?: File | null;
+  mensaje: string;
+};
+
