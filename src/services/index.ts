22b57f198e3e916780a61475cc3ee2c358489752
Carlos <dominguez5493.cd@gmail.com>
2025-03-07 14:24:11 -0500

    change name fila

--- src/services/index.ts
+++
@@ -1,3 +0,0 @@
-export {getCategorieBy, getCategories} from './getCategories'
-export {searchProducts} from './search'
-export {getOferts, getProducts, getProductsBy} from './getProducts'
--- src/services/search.ts
+++
@@ -1,8 +0,0 @@
-import { query } from "./strapi";
-
-export async function searchProducts(key: string) {
-  const res = await query(
-    `products?populate=*&filters[title][$containsi]=${key}`
-  );
-  return res.data; 
-}
