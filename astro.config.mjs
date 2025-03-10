59b53874d12edd467833475651c673d211630296
Carlos <dominguez5493.cd@gmail.com>
2025-03-10 16:28:20 -0500

    update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,17 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  output: 'server',
-  devToolbar:{
-    enabled: false
+  adapter: vercel(),
+  output: "server",
+  devToolbar: {
+    enabled: false,
   },
 });
