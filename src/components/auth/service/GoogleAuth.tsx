41204181b2ead0525848726917f238564af39450
Carlos <dominguez5493.cd@gmail.com>
2025-03-16 10:55:01 -0500

    feact: agrego componente con provider para auth con google

--- src/components/auth/service/GoogleAuth.tsx
+++ src/components/auth/service/GoogleAuth.tsx
@@ -1,10 +1,98 @@
-export const GoogleAuth = () => {
+import { GoogleOAuthProvider, useGoogleLogin } from "@react-oauth/google";
+import { useState, useEffect } from "react";
+import { googleAuth } from "@/lib/auth";
+import Cookies from "js-cookie";
+import { eventEmitter } from "@/events";
+import { handleToast } from "@/utils";
+import { Toast } from "@/components/Toast";
 
-    
+export const GoogleProvider = () => {
+  const CLIEN_ID = import.meta.env.PUBLIC_CLIENT_ID;
+
+  return (
+    <GoogleOAuthProvider clientId={CLIEN_ID}>
+      <GoogleAuth />
+    </GoogleOAuthProvider>
+  );
+};
+
+const GoogleAuth = () => {
+  const [path, setPath] = useState<string>("");
+  const [toastMessage, setToastMessage] = useState<string>("");
+  const [showToast, setShowToast] = useState<boolean>(false);
+  const [bgToast, setBgToast] = useState<string>("");
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const path = window.sessionStorage.getItem("path");
+    if (path) {
+      setPath(path);
+    } else {
+      setPath("/");
+    }
+  }, []);
+
+  const googleInit = useGoogleLogin({
+    onSuccess: async (response) => {
+      if (!response) return;
+      setLoading(true);
+      try {
+        const data = await googleAuth(response);
+        if (data) {
+          const { user, jwt } = data;
+          Cookies.set("access_token", jwt, {
+            expires: 1,
+            sameSite: "lax",
+            secure: true,
+          });
+          handleToast({
+            background: "toast-success",
+            message: "Inicio de sesion exitoso",
+            setShowToast,
+            setBgToast,
+            setToastMessage,
+          });
+          if (eventEmitter) {
+            eventEmitter.emit("changeSesion");
+            setTimeout(() => {
+              window.location.href = `${path}`;
+            }, 2000);
+          }
+        } else {
+          handleToast({
+            background: "toast-fail",
+            message:
+              "Algo salio mal con el inicio de sesion, intentalo mas tarde",
+            setShowToast,
+            setBgToast,
+            setToastMessage,
+          });
+        }
+      } catch (error) {
+        setLoading(false);
+
+        console.error("Error en el inicio de seion con proveedore externo");
+      } finally {
+        setLoading(false);
+      }
+    },
+    onError: async (response) => {
+      console.log("algo salio mal con el inicio de sesion--_>", response);
+    },
+  });
+
   return (
     <>
-      <a
-        href="http://localhost:1337/api/connect/google"
+      <Toast
+        showToast={showToast}
+        setShowToast={setShowToast}
+        toastMessage={toastMessage}
+        setToastMessage={setToastMessage}
+        bgToast={bgToast}
+        setBgToast={setBgToast}
+      />
+      <button
+        onClick={() => googleInit()}
         className="mx-auto bg-white -mb-3  text-black shadow-sm hover:bg-blue-500 rounded-lg flex items-center transition duration-150 justify-center w-9/12 gap-x-1 py-1.5 hover:text-white text-xs md:text-base font-medium"
       >
         <svg
@@ -33,8 +121,11 @@ export const GoogleAuth = () => {
             d="M43.611,20.083H42V20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z"
           ></path>
         </svg>
-        Iniciar sesion con Google
-      </a>
+        <span className="text-sm md:text-base">
+          {" "}
+          {loading ? "Validando..." : "Iniciar sesion con google"}
+        </span>
+      </button>
       <span className="text-xs md:text-sm text-gray-600 text-balance block mx-auto max-w-[80%] md:w-full">
         Inicia sesion con tu cuenta de google
       </span>
