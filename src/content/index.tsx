84c089f8cf592dab3ce192abc620a9f7d6a49360
Carlos <dominguez5493.cd@gmail.com>
2025-03-18 20:01:41 -0500

    feat: agregar metadatos para mejorar la gestión de títulos en las páginas

--- src/content/index.tsx
+++ src/content/index.tsx
@@ -1,3 +1,4 @@
 export {contenido} from './contenidoPages'
 export {getCategoriasUnicas, productos, getProductosConOfertas} from './productos'
 export {items} from './personalizados'
+export {metaData} from './meta'
---
+++ src/content/meta.ts
@@ -0,0 +1,14 @@
+export const metaData = {
+  home: {
+    title: "Bienvenido a Santas Tottas - Pasteleria Personalizada",
+  },
+  personalizados: {
+    title: "Personalizados - Pastelería a tu medida",
+  },
+  ofertas: {
+    title: "Ofertas - las mejores oferfas para que te des un gusto",
+  },
+  productos: {
+    title: "Contacto - Santas Tortas",
+  },
+};
