2213a9ba04d471a2c79cc7de04a2aa290a6f6c4f
cdomdev <dominguez5493.cd@gmail.com>
2025-03-24 22:09:46 -0500

    fix: update password field names for consistency and improve localStorage handling

--- src/components/auth/ForgotPassword.tsx
+++ src/components/auth/ForgotPassword.tsx
@@ -20,7 +20,7 @@ const FormResetPassword = ({ token }: FormResetPasswordProps) => {
   const [bgToast, setBgToast] = useState<string>("");
 
   useEffect(() => {
-    const send = localStorage.getItem("processSucces");
+    const send = localStorage.getItem("processSucces") || null;
     if (send) {
       const dataPerse = JSON.parse(send);
       setSuccess(dataPerse);
@@ -162,25 +162,26 @@ const FormResetPassword = ({ token }: FormResetPasswordProps) => {
                 <div className="relative z-0 w-full mb-5 group">
                   <Field
                     type="password"
-                    name="passwordRepeat"
+                    name="password2"
                     id="floating_repeat_password"
                     className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-[#eb9f48] focus:outline-none focus:ring-0 focus:border-[#eb9f48] peer"
                     placeholder=" "
                     required
                   />
                   <label
-                    htmlFor="passwordRepeat"
+                    htmlFor="password2"
                     className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-[#eb9f48]  peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
                   >
                     Confirma tu contraseña
                   </label>
 
                   <ErrorMessage
-                    name="passwordRepeat"
+                    name="password2"
                     component="span"
                     className="text-red-600 text-xs"
                   />
                 </div>
+
                 <button
                   type="submit"
                   className="text-black bg-secondary-bg hover:bg-[#d66a6e] uppercase  hover:text-white transition duration-200 focus:outline-none font-medium rounded-lg text-sm w-full px-5 py-2.5 text-center mb-4"
